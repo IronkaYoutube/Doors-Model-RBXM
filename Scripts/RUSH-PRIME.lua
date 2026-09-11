98cc3b95eaf55ea28ddf484a8098725822ebe292
AirSuper <149350192+AirSuper1@users.noreply.github.com>
2026-09-11 21:52:02 +0800

    Update RUSH-PRIME.lua

--- Scripts/RUSH-PRIME.lua
+++ Scripts/RUSH-PRIME.lua
@@ -2,7 +2,7 @@ local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/Regul
 
 local MyEntity = Spawner:Create({
     Entity = {
-        Name = "PRIME-RUSH",
+        Name = "RUSH-PRIME",
         Asset = "https://github.com/AirSuper1/Doors-Model-RBXM/blob/main/Entity/PRIME-RUSH.rbxm?raw=true",
         HeightOffset = 0
     },
