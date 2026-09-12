22503a110ff5bdee9eb65fb2c08be20bbcedb0ec
AirSuper <149350192+AirSuper1@users.noreply.github.com>
2026-09-12 12:40:25 +0800

    Update Depth.lua

--- Scripts/Depth.lua
+++ Scripts/Depth.lua
@@ -1,3 +1,16 @@
+local CustomAchievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/DOORS-Custom-Achievements/main/init.luau"))()
+
+CustomAchievements:Grant({
+    Identifier = "Depth",
+    Title = "Depth",
+    Desc = "Son💔🥀",
+    Reason = "Executing the Depth",
+    Image = "rbxassetid://10834791218"
+}, {
+    CheckOwned = true,
+    Remember = false
+})
+
 local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/DOORS-Entity-Spawner/refs/heads/main/init.luau"))()
 
 local MyEntity = Spawner:Create({
