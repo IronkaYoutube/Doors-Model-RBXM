eb8662a2360e38a54e6df5d4565170f711b042b9
AirSuper <149350192+AirSuper1@users.noreply.github.com>
2026-09-12 22:17:00 +0800

    Update A-235.lua

--- Scripts/A-235.lua
+++ Scripts/A-235.lua
@@ -2,9 +2,9 @@ local CustomAchievements = loadstring(game:HttpGet("https://raw.githubuserconten
 
 CustomAchievements:Grant({
     Identifier = "A-235",
-    Title = "A-235",
+    Title = "Super-Sonic",
     Desc = "SOOOO FAST",
-    Reason = "Executing the A-235",
+    Reason = "Encounter the A-235",
     Image = "rbxassetid://109914529126616"
 }, {
     CheckOwned = true,
